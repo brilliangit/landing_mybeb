21cb2c4f698cc3859840fc40253e936cf4859ca7
andri <brillianandrienw@gmail.com>
2019-10-09 06:56:41 +0700

    Add cicilan

--- application/views/cicilan.php
+++ application/views/cicilan.php
@@ -20,6 +20,88 @@
                         <div class="img-responsive">
                             <img src="<?= base_url('/assets/img/bayar-cicilan-online.jpg') ?>" alt="5 Aplikasi Pembayaran Online Terbaik di Android">
                         </div>
+                        <p>
+                            Kali ini kina tengah populer di masyarakat, “Sekarang semuanya serba kredit.” Di era sekarang, kebutuhan hidup semakin kompleks dan biaya juga semakin meroket. Tak heran jika fasilitas cicilan atau kredit kini dan bayar cicilan online1 kian populer.
+                        </p>
+                        <p>
+                            Orang yang menggunakan layanan kredit disebut dengan debitur. Ia diwajibkan membayar angsuran setiap minggu, bulan, tahun, atau sesuai ksepakatan yang dibuat dengan lembaga atau badan usaha yang memberikan fasilitas kredit.
+                        </p>
+                        <p>
+                            Jadi, jika kamu menggunakan layanan kredit untuk membeli kendaraan, maka kamu setiap bulannya wajib bayar angsuran cicilan motor, kredit mobil, cicilan rumah, pinjaman uang, dan lain-lain.
+                        </p>
+                        <h2>Keuntungan Cicilan Dibanding Tunai</h2>
+                        <p>
+                            Kenapa kamu lebih baik membeli produk dengan metode cicilan dibanding membayarkan tunai? Ternyata, membeli barang dengan metode cicilan memiliki segudang keuntungan dibanding langsung bayar secara tunai.
+                        </p>
+                        <p>
+                            <font class="bold">Pertama</font>, kamu tidak perlu menunggu sampai uangmu terkumpul guna mendapatkan barangnya. Kamu bisa mendapatkannya secara langsung dengan pembayaran yang lebih ringan.
+                        </p>
+                        <p>
+                            <font class="bold">Kedua</font>, jika kamu mau beli mobil untuk usaha, kamu bisa mendapatkannya di awal guna menjalankan usaha tersebut, tanpa harus menunggu uang kamu terkumpul banyak.
+                        </p>
+                        <p>
+                            <font class="bold">Ketiga</font>, perputaran keuangan kamu juga lebih stabil dan tidak terkurang banyak guna membeli barang dengan harga yang lumayan besar.
+                        </p>
+                        <p>
+                            Itulah setidaknya 3 keuntungan cicilan dibandingkan beli secara tunai. Adapun yang lain bisa kamu tambahkan sendiri.
+                        </p>
+                        <h2>Cara Bayar Cicilan Online di MyBeb</h2>
+                        <p>
+                            MyBeb sebagai aplikasi media sosial payment juga bisa kamu gunakan untuk bayar cicilan online1 bulanan. MyBeb telah bekerja sama secara resmi dengan beragam perusahaan penyedia cicilan, jadi kamu lebih mudah dan praktis deh. Berikut langkah-langkahnya;
+                        </p>
+                        <ol class="list" type="1">
+                            <li>Buka aplikasi Mybeb kamu. Bila belum ada, silahkan download di <font class="text-red">playstore</font></li>
+                            <li>Pilih menu <font class="bold">“Bayar”</font></li>
+                            <li>Pilih layanan <font class="bold">“Multi Finance”</font></li>
+                            <li>Pilih <font class="bold">produk cicilan</font> yang kamu gunakan</li>
+                            <li>Masukkan ID Cicilan atau Pelanggan</li>
+                            <li>Klik <font class="bold">“Lanjutkan”</font></li>
+                            <li>Lalu silahkan pilih metode pembayaran yang kamu inginkan</li>
+                            <li>Ikuti petunjuk masing-masing metode pembayaran</li>
+                        </ol>
+                        <p>
+                            Setelah pembayaran sukses, akan ada notifikasi via aplikasi, email dan struk pembayaran
+                        </p>
+                        <h2>Daftar Perusahaan Multi Finance di MyBeb</h2>
+                        <p>
+                            Berikut adalah daftar perusahaan Multi Finance yang telah bekerja sama dengan MyBeb dalam hal <font class="text-blue">bayaran cicilan secara online</font>;
+                        </p>
+                        <ul class="list">
+                            <li>Mega Finance</li>
+                            <li>BAF Auto Finance</li>
+                            <li>BIMA Finance</li>
+                            <li>FIF</li>
+                            <li>Mega Auto Finance</li>
+                            <li>Mega Central Finance</li>
+                            <li>Woka Finance</li>
+                            <li>WOM Finance</li>
+                        </ul>
+                        <p>
+                            Bila perusahaan penyedia angsuran yang kamu cari tidak ada, silahkan laporkan ke CS MyBeb agar kami segera menerbitkan atau menjalin kerja sama.
+                        </p>
+                        <h2>FAQ Seputar Cicilan</h2>
+                        <h6>Q : Apa Saja Cicilan Kredit yang Bisa Dibayar di MyBeb?</h6>
+                        <p>
+                            A : Saat ini MyBeb telah bekerja sama dengan 8 perusahaan penyedia cicilan kredit, kamu bisa melihatnya di aplikasi MyBeb. Pembayaran cicilan MyBeb jauh lebih praktis dan menguntungkan karena selalu ada promo dan cashback buat kamu.
+                        </p>
+                        <h6>Q : Kapan Bisa Bayar Cicilan di MyBeb?</h6>
+                        <p>
+                            A : Kamu bisa <font class="text-blue">bayar cicilan online</font> perusahaan leasing Indonesia kapan saja di MyBeb. Sistem pembayaran di MyBeb beroperasi selama 24 jam setiap hari. Bayar tagihan kamu sebelum tanggal jatuh tempo perjanjian kontrak. Jika kamu lupa silakan cek di aplikasi MyBeb lalu masukan nomor kontrak kamu.
+                        </p>
+                        <h6>Q : Bagaimana Jika Telat Membayar Cicilan?</h6>
+                        <p>
+                            A : Aplikasi MyBeb tetap melayani pembayaran yang terlambat. Rincian angsuran yang belum dibayarkan akan diterbitkan oleh sistem dan kamu bisa memilih angsuran periode mana yang ingin dibayar terlebih dahulu.
+                        </p>
+                        <p>
+                            Beberapa perusahaan penyedia jasa kredit memberikan pinalti atas keterlambatan berupa denda, sehingga kamu wajib membayar denda bila telat membayar cicilan.
+                        </p>
+                        <h6>Q : Berapa Bunga dan Denda Dari Keterlembatatan?</h6>
+                        <p>
+                            A : Setiap perusahaan menetapkan bunga cicilan dan denda keterlambatan yang berbeda-beda. Untuk mengetahui lebih lanjut, silakan menghubungi pihak perusahaan yang memberikan layanan pada kamu.
+                        </p>
+                        <p>
+                            ups, demikian ulasan tentang <font class="text-blue">cek dan bayar cicilan Online</font>, dan jangan lupa bayar tagihan kamu lainnya ya, seperti <font class="text-red">bayar tagihan listrik PLN, tagihan PDAM, iuran BPJS Kesehatan dan top up saldo Gopay serta Saldo OVO</font>.
+                        </p>
                     </div>
                 </div>
             </div>
