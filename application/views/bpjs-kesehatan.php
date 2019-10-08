4ac6f13cd67af6a85b3ac566b113b74ee597c19a
andri <brillianandrienw@gmail.com>
2019-10-09 06:14:28 +0700

    add BPJS

--- application/views/bpjs-kesehatan.php
+++ application/views/bpjs-kesehatan.php
@@ -20,6 +20,88 @@
                         <div class="img-responsive">
                             <img src="<?= base_url('/assets/img/bayar-bpjs-kesehatan.jpg') ?>" alt="5 Aplikasi Pembayaran Online Terbaik di Android">
                         </div>
+                        <p>
+                            <font class="text-red">MyBeb</font> - Sejak diluncurkannya BPJS Kesehatan, kini masyarakat dengan ekonomi menengah ke bawah tetap menerima layanan kesehatan yang baik dan terjamin. Tidak ada lagi rasa takut untuk periksa ke dokter atau menjalani perawatan di rumah sakit, karena semua sudah ditanggung oleh BPJS Kesehatan.
+                        </p>
+                        <p>
+                            Dengan biaya yang relatif terjangkau, BPJS telah berubah menjadi kebutuhan siapa saja. Tidak hanya masyarakat dengan ekonomi menengah ke bawah, tak sedikit orang kaya yang tetap mendaftarkan dirinya di BPJS Kesehatan untuk kesejahteraan hidupnya.
+                        </p>
+                        <p>
+                            Karenanya, sebagai bentuk rasa terimakasih kita pada pemerintah atas layanan yang luar biasa ini, mari kita upayakan <font class="text-blue">bayar BPJS Kesehatan</font> secara tepat waktu. Cukup dengan install aplikasi pembayaran online, kini kamu sudah bisa bayar iuran kapanpun dan dimanapun.
+                        </p>
+                        <h2>
+                            Yuk Bayar BPJS Kesehatan di MyBeb
+                        </h2>
+                        <p>
+                            Bayar iuran BPJS Kesehatan di MyBeb bukan hanya dapat asuransi kesehatan saja loh, kamu juga berkesempatan dapat 5 voucher gratis premium dari MyBeb. Wow, keren banget kan? Yuk intip apa saja voucher gratis yang bisa kamu dapatkan dengan bayar BPJS Kesehatan di MyBeb.
+                        </p>
+                        <h2>Voucher KFC Rp.50.000</h2>
+                        <p>
+                            Kamu penggemar ayam goreng KFC? Wah, pas banget kalau gitu. Dengan aktif menggunakan aplikasi MyBeb dan bayar semua tagihan di MyBeb, kamu bisa dapatkan voucher KFC senilai Rp.50.000 gratis. Nyammi.
+                        </p>
+                        <h2>Voucher McD Rp.100.000</h2>
+                        <p>
+                            Oh ternyata kamu lebih suka McD daripada KFC? Baiklah, MyBeb sudah menyediakan voucher sebesar Rp.100.000 untuk kamu semua yang aktif chattingan, post status, foto atau video di media sosial MyBeb dan bayar semua tagihan di MyBeb juga. Bakal seru banget kan?
+                        </p>
+                        <h2>Paket Liburan ke Bromo</h2>
+                        <p>
+                            Kamu ingin menikmati indahnya panorama sunrise Bromo? Berlarian di atas hamparan savana? Atau menikmati keindahan Bukit Teletubbies? Gampang banget kok. Dengan bayar tagihan BPJS Kesehatan dan aktif menggunakan MyBeb, kamu bisa mendapatkan kesempatan jalan-jalan ke Bromo gratis tis tis. Mau?
+                        </p>
+                        <h2>Paket Liburan ke Jogja</h2>
+                        <p>
+                            Udah bosan ke Bromo? Okedeh, kan ada Jogja. Nah, siapa sih yang tidak kangen dengan gemuruh musik keroncong di Malioboro? Menikmati hangatnya ronde sambil melihat bapak-bapak mengayuh becaknya? Itu kan Jogja banget!
+                        </p>
+                        <p>
+                            Yuk chatting Doi pakai Mybeb dan bayar semua tagihan kamu di MyBeb. Dengan terus aktif menggunakan MyBeb kamu berpeluang mendapatkan CINTA yang begitu banyak. Nah, dengan CINTA itu kamu bisa liburan gratis deh ke Jogja dengan Bebeb.
+                        </p>
+                        <h2>Liburan Spesial ke Singapura</h2>
+                        <p>
+                            Kak, aku udah bosan nih liburan ke dalam negeri mulu. Sesekali ke luar negeri dong? Baik, baik. MyBeb bakal menuruti kemauan kamu kok. Dengan berbekal 1,5 juta CINTA, kamu bisa berangkat ke Singapura dan liburan spesial disana secara gratis tis tis. Mantuel banget kan?
+                        </p>
+                        <h2>Cek & Bayar BPJS Kesehatan di MyBeb</h2>
+                        <p>
+                            Berikut adalah panduan bayar BPJS Kesehatan di aplikasi MyBeb.
+                        </p>
+                        <ol class="list" type="1">
+                            <li>Buka <font class="bold">aplikasi Mybeb</font> kamu. Bila belum ada, silahkan download di playstore</li>
+                            <li>Pilih layanan <font class="bold">“BPJS”</font></li>
+                            <li>Masukkan <font class="bold">“Nomor Polis/ID Pelanggan”</font></li>
+                            <li>Masukkann nomor HP kamu</li>
+                            <li>Pilih jumlah bulan</li>
+                            <li>Klik <font class="bold">“Lanjutkan”</font> dan pilih metode pembayaran</li>
+                            <li>Ikuti petunjuk masing-masing metode pembayaran</li>
+                            <li>Cek terlebih dulu kesesuaian data</li>
+                            <li>Setelah pembayaran sukses, akan ada notifikasi via aplikasi, email dan struk pembayaran</li>
+                        </ol> 
+
+                        <h2>Cara Daftar BPJS Kesehatan</h2>
+                        <p>
+                            Kini daftar layanan BPJS Kesehatan tidak perlu mengantre lagi, karena pendaftaran bisa dilakukan via online. Akses situs resmi BPJS Kesehatan, kemudian cari kolom layanan dan pilih pendaftaran peserta.
+                        </p>
+                        <p>
+                            Baca terlebih dahulu syarat dan ketentuannya. Kamu perlu menyiapkan beberapa berkas untuk daftar secara Online, antara lain: Kartu Tkamu Penduduk (KTP), Kartu Keluaga (KK), Kartu Nomor Pokok Wajib Pajak (NPWP), alamat e-mail aktif, dan nomor telepon yang dapat dihubungi.
+                        </p>
+                        <p>
+                            Isi data pribadi pada kolom yang tersedia. Setelah selesai, pilih ‘Simpan.’ Setelah itu tentukan besaran iuran sesuai dengan kelas perawatan kamu dipilih.
+                        </p>
+                        <p>
+                            Setelah semua data dilengkapi, sistem akan secara otomatis mengirimkan email ke akun email yang kamu daftarkan berisi Nomor Registrasi.
+                        </p>
+                        <p>
+                            Selanjutnya kamu wajib untuk melakukan pembayaran di bank sesuai dengan nomor virtual account yang diberikan. Bisa melalui Bank Mandiri, BRI, dan BNI
+                        </p>
+                        <p>
+                            Kamu akan menerima email berisi e-ID berisi link. Silakan print e-ID tersebut karena e-ID tersebut berfungsi seperti kartu peserta BPJS Kesehatan. Kartu ini bisa kamu bawa saat periksa atau berobat ke rumah sakit atau puskemas dan jangan lupa bawa juga KTP atau Kartu Keluarga.
+                        </p>
+                        <p>
+                            Kartu BPJS Kesehatan dalam bentuk fisik bisa kamu ambil setelah 7 hari kerja. Saat mengambil kartu BPJS Kesehatan, bawa dokumen KTP (asli dan fotokopi), fotokopi Kartu Keluarga, foto berwarna ukuran 3x4 (2 lembar), serta cetak formulir pendaftaran online beserta bukti pembayaran bank.
+                        </p>
+                        <p>
+                            Untuk kamu yang pekerja di swasta, pendaftaran BPJS Kesehatan juga bisa dilakukan secara kolektif oleh perusahaan. Jadi, kamu tidak perlu mengurus semuanya sendiri untuk daftar di BPJS Kesehatan.
+                        </p>
+                        <p>
+                            Yups, itulah sekilas ulasan tentang <font class="text-blue">bayar BPJS Kesehatan</font> di MyBeb. Cek dan bayar juga tagihan lainnya di aplikasi MyBeb agar CINTA kamu makin banyak, seperti tagihan listrik, tagihan PDAM, beli voucher game, dan beli pullsa gratis.
+                        </p>
                     </div>
                 </div>
             </div>
