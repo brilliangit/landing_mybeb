5236cef5f73f7d96f642264a6cb65a677730a751
Brillian Andrie <mygithub.svn@gmail.com>
2019-10-09 11:57:40 +0700

    fix indihome

--- application/views/indihome.php
+++ application/views/indihome.php
@@ -26,6 +26,91 @@
                         <p>
                             Karenanya tak heran jika tiap kali ada warung free WiFi provider yang digunakan selalu Indihome. Pemasangan yang mudah, biaya pemasangan yang terjangkau dan paket internet yang beragam, tak heran jika produk dari Telkom Indonesia kini menjadi primadona hampir seluruh warung di Indonesia.
                         </p>
+                        <h2>Seputar Indihome</h2>
+                        <p>
+                            Indonesia Digital Home atau yang akrab disebut Indihome, merupakan layanan komunikasi data multimedia yang dipelopori oleh PT. Telekomunikasi Indonesia atau Telkom yang secara resmi diluncurkan pada tahun 2015.
+                        </p>
+                        <p>
+                            Indihome awalnya adalah satu proyek utama Telkom yang masuk ke Indonesia Digital Network 2015 silam. Dalam proyek ini Telkom bermitra dengan sejumlah perusahaan teknologi informasi untuk membangun rumah berkonsep digital.
+                        </p>
+                        <p>
+                            Indihome menawarakan layanan 3 in 1, 3 layanan dalam 1 paket langganan. Paket ini terdiri dari saluran telepon rumah, internet dan televisi interaktif. Tidak hanya itu, Indihome juga menyediakan layanan lain yang bisa dipilih oleh pelangganannya.
+                        </p>
+                        <p>
+                            Beberapa layanan tambahan itu diantaranya telepon mania, seamless, Wifi.id, TrenMicro Antivirus, dan Indihome View. Paket tambahan ini juga dilengkapi dengan konten portal musik digital dan Home Automation.
+                        </p>
+                        <h2>Indihome Coverage</h2>
+                        <p>
+                            Saat ini Indihome Fiber telah menyediakan jangkauan hampir seluruh wilayah di Indonesia, mulai dari Sumatra sampai Papua. Jadi hampir semua kabupaten kota di Indonesia bisa memasang jaringan internet lewat Indihome.
+                        </p>
+                        <p>
+                            Terlebih di kota-kota besar, seperti JABODETABEK, Karawang, Bandung, Indramayu, Cirebon, Tegal, Kendal, Pekalongan, Semarang, Ungaran, Salatiga, Klaten, Solo, Jogja, Madiun, Kediri, Blitar, Sidoarjo, Surabaya, Malang, Lumajang, Jember, termasuk di Madura. Untuk edisi lengkapnya kamu bisa mengakses di laman https://fibermap.indihome.co.id
+                        </p>
+                        <h2>Cek dan Bayar Tagihan Indihome di MyBeb</h2>
+                        <p>
+                            Sebagai aplikasi pembayaran digital terlengkap, MyBeb juga menyediakan layanan cek dan bayar tagihan Indihome1. Sehingga seluruh pengguna MyBeb yang berlangganan Indihome bisa bayar tagihan lebih mudah. Berikut langkah-langkahnya:
+                        </p>
+                        <ol class="list" type="1">
+                            <li>Buka <font class="bold">aplikasi Mybeb</font>. Bila belum ada, silahkan download di <font class="text-red">playstore</font></li>
+                            <li>Pilih menu <font class="bold">“Bayar”</font></li>
+                            <li>Pilih layanan <font class="bold">“Indihome”</font></li>
+                            <li>Masukkan <font class="bold">ID Indihome</font> kamu</li>
+                            <li>Klik <font class="bold">“Lanjutkan”</font></li>
+                            <li>Setelah detail tagihan muncul, klik <font class="bold">“Bayar”</font></li>
+                            <li>Lalu silahkan pilih metode pembayaran yang kamu inginkan</li>
+                            <li>kuti petunjuk masing-masing metode pembayaran</li>
+                        </ol>
+                        <p>
+                            Setelah pembayaran sukses, akan ada notifikasi via aplikasi, email dan struk pembayaran
+                        </p>
+                        <h2>Cara Berlangganan Indihome</h2>
+                        <p>
+                            Apabila kamu ada rencana pasang Wifi di rumah atau di tempat usahamu, kamu bisa berlangganan internet di Indihome. Berikut panduannya:
+                        </p>
+                        <ol class="list" type="1">
+                            <li>Silahkan hubungi Customer Service Indihome untuk melakukan pendaftaran. Namun bila kamu kesulitan, silahkan search di Google dengan keyword “Jasa Pasang Indihome Surabaya”. Nama kota sesuaikan dengan daerah kamu</li>
+                            <li>Kontak penyedia jasa yang telah kamu temukan via Internet dan berkonsultasilah mengenai paket Internet yang disediakan. Biasanya penyedia jasa pasang Indihome merupakan pegawai Indihome sendiri.</li>
+                            <li>Lengkapi formulir untuk pendaftaran, berupa KTP asli dan alamat pemasangan lengkap. Biasanya kamu hanya diminta mengirimkan foto KTP dan data lengkap via WhatsApp.</li>
+                            <li>Tentukan pilihan paket Indihome yang kamu inginkan. Untuk detail paketnya akan kami jelaskan di bawah.</li>
+                            <li>Setelah data berhasil terkirim, pihak Telkom pusat akan menghubungi kamu untuk memverifikasi kebenaran data. Setelah itu akan ada petugas yang akan mensurvei dan melakukan pemasangan jaringan internet ke tempat kamu.</li>
+                        </ol>
+                        <h2>Paket Internet Indihome</h2>
+                        <p>
+                            Berdasarkan data yang tersedia di aplikasi MyIndihome, ada beberapa paket layanan yang bisa kamu gunakan.
+                        </p>
+                        <h4>Paket Dual Play</h4>
+                        <p>
+                            Layanan internet cepat via fiber optik<br>
+                            Bebas telepon hingga 300 menit
+                        </p>
+                        <p>Untuk layanan internetnya ada 2 pilihan</p>
+                        <ul class="list">
+                            <li>20 Mbps : Rp.380.000 / bulan</li>
+                            <li>30 Mbps : 525.000 / bulan</li>
+                        </ul>
+                        <h4>Paket Triple Play</h4>
+                        <p>
+                            Layanan internet cepat via fiber optik<br>
+                            152 channel UseeTV dan Video in Demand di layar TV kamu<br>
+                            Bebas telepon hingga 1000 menit
+                        </p>
+                        <p>
+                            Untuk layanan internetnya ada 6 pilihan
+                        </p>
+                        <ul class="list">
+                            <li>10 Mbps : Rp.435.000 / bulan</li>
+                            <li>20 Mbps : Rp.555.000/ bulan</li>
+                            <li>30 Mbps : Rp.735.000 / bulan</li>
+                            <li>40 Mbps : Rp.930.000 / bulan</li>
+                            <li>50 Mbps : Rp.1.225.000 / bulan</li>
+                            <li>100 Mbps : Rp.1.890.000 / bulan</li>
+                        </ul>
+                        <p>
+                            Kamu bisa memilih layanan di atas sesuai dengan kebutuhan kamu. Biasanya Indihome juga menyediakan promo setiap bulannya, khususnya untuk paket Dual Play. Dan untuk standar warung free Wifi, paket Dual Play dengan kapasitas internet 10 Mbps sudah cukup, bila kurang bisa diupgrade ke 20 Mbps.
+                        </p>
+                        <p>
+                            Demikian ulasan seputar <font class="bold">cek dan bayar tagihan Indihome</font> terlengkap. Jangan lupa bayar tagihan kamu lainnya di aplikasi Mybeb, seperti <font class="bold">tagihan listrik, tagihan PDAM, bayar cicilan</font>, maupun <font class="bold">iuran BPJS Kesehatan</font>.
+                        </p>
                     </div>
                 </div>
             </div>
