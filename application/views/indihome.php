08309c71d0a0017ae085dc9954206616af6bcfcc
andri <brillianandrienw@gmail.com>
2019-10-09 07:22:27 +0700

    Add indihome

---
+++ application/views/indihome.php
@@ -0,0 +1,34 @@
+<section class="uza-portfolio-area mt-30">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="text-center">
+                    <h1 class="text-headline">
+                        <!--this title-->
+                        Cek dan Bayar Tagihan Indihome Auto Bonus
+                    </h1>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid">
+        <div class="row justify-content-md-center">
+            <div class="col-lg-7 mb-30">
+                <div class="card">
+                    <div class="card-body">
+                        <!--This content-->
+                        <div class="img-responsive">
+                            <img src="<?= base_url('/assets/img/indihome.jpg') ?>" alt="Cek dan Bayar Tagihan Indihome Auto Bonus">
+                        </div>
+                        <p>
+                            Kini Indihome menjadi salah satu profider penyedia jaringan internet yang banyak diminati masyarakat. Berkat adanya trend warung free WiFi popularitas Indihome di masyarakat desa kian melejit. Selain menawarkan paket internet yang terjangkau, masyarakat juga lebih mudah dalam <font class="bold">bayar tagihan Indihome</font>, sebab bayar tagihan kini bisa dilakukan secara online.
+                        </p>
+                        <p>
+                            Karenanya tak heran jika tiap kali ada warung free WiFi provider yang digunakan selalu Indihome. Pemasangan yang mudah, biaya pemasangan yang terjangkau dan paket internet yang beragam, tak heran jika produk dari Telkom Indonesia kini menjadi primadona hampir seluruh warung di Indonesia.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
