03b23574fe5a3b004e9955535ebdefe83abc5188
andri <brillianandrienw@gmail.com>
2019-10-10 09:13:42 +0700

    fixing page by kategory

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -28,37 +28,53 @@ class MY_Controller extends CI_Controller {
                 $head['meta_descriptions'] = 'Jika kamu ingin bayar tagihan secara online, berikut adalah 5 aplikasi pembayaran online terbaik di Android. Yuk segera download dan bayar tagihanmu';
                 $head['title_page'] = 'MyBeb - Aplikasi pembayaran online';
                 break;
-            case 'ppob':
+            case 'bisnis/ppob':
                 $head['meta_descriptions'] = 'Jika kamu mencari aplikasi bisnis PPOB terbaik di Android, baca ulasan ini dan temukan beragam info aplikasi bisnis pembayaran terlengkap.';
                 $head['title_page'] = 'MyBeb - Aplikasi Bisnis PPOB Terbaik di Android';
                 break;
-            case 'pulsa':
+            case 'bisnis/pulsa':
                 $head['meta_descriptions'] = 'MyBeb adalah aplikasi jualan pulsa termurah dan terbaik di Android. Harganya termurah dan penggunaannya gratis tanpa biaya lisensi.';
                 $head['title_page'] = 'MyBeb - Aplikasi Jualan Pulsa Termurah di Android';
                 break;
-            case 'bpjs-kesehatan':
+            case 'bisnis/sampingan':
+                $head['meta_descriptions'] = 'Ada banyak pilihan bisnis sampingan anak sekolah yang bisa kamu jumpai di artikel ini. Yuk baca segera dan temukan bisnis apa yang cocok buat kamu.';
+                $head['title_page'] = 'MyBeb - Bisnis Sampingan Anak Sekolah, Pasti Sukses';
+                break;
+            case 'tagihan/bpjs-kesehatan':
                 $head['meta_descriptions'] = 'Yuk bayar BPJS Kesehatan di MyBeb sekarang, dan dapatkan bonus voucher liburan gratis ke Jogjakarta. Ayo cek dan bayar segera!';
                 $head['title_page'] = 'MyBeb - Bayar BPJS Kesehatan di MyBeb Bonus Liburan Gratis';
                 break;
-            case 'cicilan':
+            case 'tagihan/cicilan':
                 $head['meta_descriptions'] = 'Yuk bayar cicilan online di MyBeb dan dapatkan voucher McD sebesar 100 RIBU. Bayar angsuran kamu segera ya dan raih vouchernya.';
                 $head['title_page'] = 'MyBeb - Bayar Cicilan Online di MyBeb Bonus Voucher McD';
                 break;
-            case 'ovo':
-                $head['meta_descriptions'] = 'Yuk top up saldo OVO kamu di MyBeb sekarang, ada voucher KFC senilai 50.000 gratis. Ayo isi saldo OVO kamu sekarang!.';
-                $head['title_page'] = 'MyBeb - Top Up Saldo OVO - Grab - Bonus Voucher McD 100.000';
-                break;
-            case 'gopay':
-                $head['meta_descriptions'] = 'Yuk top up saldo Gopay kamu di MyBeb sekarang, ada voucher KFC senilai 50.000 gratis. Ayo isi saldo Gopay kamu sekarang!.';
-                $head['title_page'] = 'MyBeb - Top Up Saldo Gopay - Bonus Voucher KFC 50.000';
-                break;
-            case 'dana':
-                $head['meta_descriptions'] = 'Yuk top up saldo DANA kamu di MyBeb sekarang, ada voucher McD senilai 50.000 gratis. Ayo isi saldo DANA sekarang!';
-                $head['title_page'] = 'MyBeb - Top Up Saldo DANA - TIX ID di MyBeb';
-            case 'indihome':
+            case 'tagihan/indihome':
                 $head['meta_descriptions'] = 'Yuk segera cek dan bayar tagihan Indihome kamu di aplikasi MyBeb. Ada banyak bonus dan voucher gratis yang bisa kamu dapatkan, gratis.';
                 $head['title_page'] = 'MyBeb - Cek dan Bayar Tagihan Indihome Auto Bonus';
                 break;
+            case 'tagihan/kartu-kredit':
+                $head['meta_descriptions'] = 'Hari ini bayar tagihan kartu kredit bisa dilakukan di aplikasi MyBeb. Bayar tagihan kartu kredit jadi lebih mudah dan dapat voucher gratis.';
+                $head['title_page'] = 'MyBeb - Bayar Tagihan Kartu Kredit di MyBeb';
+                break;
+            case 'topup/ovo':
+                $head['meta_descriptions'] = 'Yuk top up saldo OVO kamu di MyBeb sekarang, ada voucher KFC senilai 50.000 gratis. Ayo isi saldo OVO kamu sekarang!.';
+                $head['title_page'] = 'MyBeb - Top Up Saldo OVO - Grab - Bonus Voucher McD 100.000';
+                break;
+            case 'topup/gopay':
+                $head['meta_descriptions'] = 'Yuk top up saldo Gopay kamu di MyBeb sekarang, ada voucher KFC senilai 50.000 gratis. Ayo isi saldo Gopay kamu sekarang!.';
+                $head['title_page'] = 'MyBeb - Top Up Saldo Gopay - Bonus Voucher KFC 50.000';
+                break;
+            case 'topup/dana':
+                $head['meta_descriptions'] = 'Yuk top up saldo DANA kamu di MyBeb sekarang, ada voucher McD senilai 50.000 gratis. Ayo isi saldo DANA sekarang!';
+                $head['title_page'] = 'MyBeb - Top Up Saldo DANA - TIX ID di MyBeb';
+            case 'voucher/joox':
+                $head['meta_descriptions'] = 'Ini adalah cara mendapatkan joox VIP secara langsung dan paling gampang. Cukup degan install MyBeb, langsung dapat JOOX VIP.';
+                $head['title_page'] = 'MyBeb - Cara Mendapatkan JOOX VIP';
+                break;
+//            case 'joox':
+//                $head['meta_descriptions'] = 'Yuk segera install MyBeb dan inilah cara mendapatkan saldo OVO gratis paling gampang. Segera install dan raih saldo sebanyak-banyaknya.';
+//                $head['title_page'] = 'MyBeb - Cara Mendapatkan Saldo OVO Gratis';
+//                break;
             default:
                 $head['meta_descriptions'] = 'Ini meta desc default';
                 $head['title_page'] = 'Ini title default';
